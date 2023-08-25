65f0514897ca43295ba684a1c94d733acc1c1765
heba892 <hebagamal231199@gmail.com>
2023-08-25 16:23:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import DashboardView from './components/DashboardView';
+import Sidebar from './components/Sidebar';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="">
+      <div className="flex overflow-scroll ">
+        <div className="basis-[12%] h-[100vh]">
+          <Sidebar />
+        </div>
+        <div className="basis-[88%] border overflow-scroll h-[100vh]">
+                  <DashboardView />
+
+          <div>
+                      <Outlet></Outlet>
+
+          </div>
+        </div>
+
+
+      </div>
     </div>
   );
 }
